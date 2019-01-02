2e7cadf791af34b8d84823aa793e832b41f81651
Pirated Virus <saurabhk201@gmail.com>
2019-01-02 22:26:24 +0530

    Added new feature: Stick orginial question to original place and duplicate question into new frame

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-var div = document.getElementById('div_id'),
-    clone = div.cloneNode(true); // true means clone all childNodes and all event handlers
-clone.id = "some_id";
-document.body.appendChild(clone);
+// var div = document.getElementById('div_id'),
+//     clone = div.cloneNode(true); // true means clone all childNodes and all event handlers
+// clone.id = "some_id";
+// document.body.appendChild(clone);
 
 
-$(".qa-q-view-content").clone().insertAfter("div.qa-view-count");
+// $(".qa-q-view-content").clone().insertAfter("div.qa-view-count");
 
 
 
 
-$(document).ready(function () {
-        var clone = $(".qa-q-view-content").clone().attr('id','newID');        
-        console.log(clone);
-        $(clone).insertAfter("div.qa-view-count");
-});
+// $(document).ready(function () {
+//         var clone = $(".qa-q-view-content").clone().attr('id','newID');        
+//         console.log(clone);
+//         $(clone).insertAfter("div.qa-view-count");
+// });
+
+void function () {
+        var e = document.createElement('div');
+        var t = document.getElementsByClassName('qa-q-view-content')[0];
+        e.className = t.className, e.innerHTML = t.innerHTML,
+                e.style.cssText += ';position:fixed;top:2px;right:2px;z-index:9999;border:2px solid black;resize:both;width:30%;height:auto', document.body.append(e)
+}();
